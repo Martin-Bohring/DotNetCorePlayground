c94d264f6df974f2c21ec91d33be5e8c58db795f
Martin Bohring <martin.bohring@shinternet.ch>
2017-02-12 16:06:01 +0100

    Got a first test running using Fixie 2 alpha version

---
+++ MediaService/tests/Media.Domain.Tests/DependencyTests.cs
@@ -0,0 +1,24 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace Media.Domain.Tests
+{
+    using Shouldly;
+
+    public sealed class DependencyTests
+    {
+        [Input("AutoFixture")]
+        [Input("AutoFixture.Idioms")]
+        [Input("Media.Domain.Tests")]
+        [Input("Shouldly")]
+        [Input("Fixie")]
+        public void MediaDomainShouldNotReference(string assemblyName)
+        {
+            var references = typeof(ValueObject<>).Assembly.GetReferencedAssemblies();
+
+            references.ShouldNotContain(reference => reference.Name == assemblyName);
+        }
+    }
+}
---
+++ MediaService/tests/Media.Domain.Tests/UnitTestConvention.cs
@@ -0,0 +1,32 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace Media.Domain.Tests
+{
+    using System.Linq;
+
+    using Fixie;
+    using Shouldly;
+
+    public class UnitTestConvention : Convention
+    {
+        public UnitTestConvention()
+        {
+            Classes
+                .NameEndsWith("Tests")
+                .Where(type => type.GetConstructors()
+                .All(constructorInfo => !constructorInfo.GetParameters().Any()));
+
+            Methods.Where(method => method.IsPublic && method.IsVoid());
+
+            Parameters
+                .Add<InputAttributeParameterSource>();
+
+            HideExceptionDetails
+                        .For<ShouldAssertException>()
+                        .For<ShouldCompleteInException>();
+        }
+    }
+}
